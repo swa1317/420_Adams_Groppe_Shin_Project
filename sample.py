028e59be7c44534a63677a7d60f8137fd9d5c5e2
Bahdah Shin <bahdahdev@gmail.com>
2020-11-28 18:30:33 -0500

    checks for float in string

---
+++ sample.py
@@ -0,0 +1,9 @@
+
+# https://docs.novatel.com/OEM7/Content/Logs/GPRMC.htm
+# https://docs.novatel.com/OEM7/Content/Logs/GPGGA.htm
+# https://docs.novatel.com/OEM7/Content/Logs/GPGSA.htm
+# https://docs.novatel.com/OEM7/Content/Logs/GPVTG.htm
+
+if __name__ == '__main__':
+    a = "1.1"
+    print(a.isdecimal())
