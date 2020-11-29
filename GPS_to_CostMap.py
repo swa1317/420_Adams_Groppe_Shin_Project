d21502190e68b3470362310f76fc049d9e619bc5
Bahdah Shin <bahdahdev@gmail.com>
2020-11-29 16:01:04 -0500

    angle and direction

--- GPS_to_CostMap.py
+++ GPS_to_CostMap.py
@@ -188,9 +188,24 @@ def write_kml(lines_kml_body, found_stops, output_file):
     f.write(kml_last_two)
     f.close()
 
-def getAngle(a, b, c):
-    angle = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
-    return angle + 360 if angle < 0 else angle
+def getAngle(p_1, p_2, p_3):
+    l_1_2 = math.sqrt((p_1[0]-p_2[0])**2 + (p_1[1]-p_2[1])**2)
+    l_2_3 = math.sqrt((p_2[0]-p_3[0])**2 + (p_2[1]-p_3[1])**2)
+    l_3_1 = math.sqrt((p_3[0]-p_1[0])**2 + (p_3[1]-p_1[1])**2)
+    return math.degrees(math.cos(((l_2_3)**2 + (l_1_2)**2 - (l_3_1)**2) / (2 * l_2_3 * l_1_2)))
+
+def getDirection(p_1, p_2, p_3):
+    v1 = (p_2[0]-p_1[0], p_2[1]-p_1[1])
+    v2 = (p_2[0]-p_3[0], p_2[1]-p_3[1])
+    cross = v1[1]*v2[0] - v1[0]*v2[1]
+    if cross > 0:
+        return "left"
+    if cross < 0:
+        return "right"
+    dot = v1[1]*v2[1] + v1[0]*v2[0]
+    if dot > 0:
+        return "straight"
+    return "uturn"
 
 if __name__ == '__main__':
     parameter = sys.argv[1:]
