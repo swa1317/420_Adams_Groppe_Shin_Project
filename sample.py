aa3d97fb6f801f338976eb4b2e784cb44c671e5f
Bahdah Shin <bahdahdev@gmail.com>
2020-11-29 13:41:02 -0500

    added direction

--- sample.py
+++ sample.py
@@ -10,11 +10,20 @@ def getAngle(p_1, p_2, p_3):
     l_1_2 = math.sqrt((p_1[0]-p_2[0])**2 + (p_1[1]-p_2[1])**2)
     l_2_3 = math.sqrt((p_2[0]-p_3[0])**2 + (p_2[1]-p_3[1])**2)
     l_3_1 = math.sqrt((p_3[0]-p_1[0])**2 + (p_3[1]-p_1[1])**2)
+    return math.degrees(math.cos(((l_2_3)**2 + (l_1_2)**2 - (l_3_1)**2) / (2 * l_2_3 * l_1_2)))
 
-    a = math.degrees(math.cos(((l_2_3)**2 + (l_1_2)**2 - (l_3_1)**2) / (2 * l_2_3 * l_1_2)))
-
-    print(a)
-    return 0
+def getDirection(p_1, p_2, p_3):
+    v1 = (p_2[0]-p_1[0], p_2[1]-p_1[1])
+    v2 = (p_2[0]-p_3[0], p_2[1]-p_3[1])
+    cross = v1[1]*v2[0] - v1[0]*v2[1]
+    if cross > 0:
+        return "left"
+    if cross < 0:
+        return "right"
+    dot = v1[1]*v2[1] + v1[0]*v2[0]
+    if dot > 0:
+        return "straight"
+    return "uturn"
 
 if __name__ == '__main__':
     a = (1,1)
@@ -22,7 +31,8 @@ if __name__ == '__main__':
     c = (2, 3)
     d = (3, 2)
 
-    e = getAngle(a,b,c)
-    f = getAngle(a,b,d)
-    print(e)
-    print(f)
+    print(getAngle(a,b,c))
+    print(getAngle(a,b,d))
+
+    print(getDirection(a,b,c))
+    print(getDirection(a,b,d))
